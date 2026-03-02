605dad57922eaaefdfc3766ad97c69d68c2ca231
ClioTorbenson <lc22-0873@lclark.edu>
2026-03-02 13:20:39 -0800

    Add files via upload
    opens sql file, will go through all rows instead of breaking at 100

--- tree-data.py
+++ tree-data.py
@@ -6,13 +6,9 @@ with open('Street_Tree_Inventory_-_Active_Records.csv') as f:
   tree_data = ''
   site_data = ''
   location_data = ''
-  i = 0
   for row in csv_reader:
     tree_data += 'INSERT INTO tree (species, mature_size, functional_type, diameter, tree_condition, date_inventoried) \n VALUES (\'' + row[6] + '\', \'' + row[7] + '\', \'' + row[8] + '\', ' + str(round(float(row[9]), 1)) + ', \'' + row[10] + '\', \'' + row[5].replace('/', '-')[:-12] + '\');' + '\n'
     site_data += 'INSERT INTO site (size, width, wires, improvement, site_type) \n VALUES (\'' + row[12] + '\', ' + str(round(float(row[13]), 1)) + ', \'' + row[14]  + '\', \'' + row[15] + '\', \'' + row[11] + '\'); \n'
     location_data += 'INSERT INTO location VALUES (\'' + row[4] + '\', \'' + row[3] + '\'' + '); \n'
-    i += 1
-    if i == 100:
-        break
-with open('tree-data.txt', 'w') as fil:
+with open('tree-data.sql', 'w') as fil:
     print('USE tree; \n' + tree_data + site_data + location_data, file=fil)
